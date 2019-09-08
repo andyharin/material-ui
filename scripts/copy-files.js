b7e6e204cf7085e62b4f40fabc1f273ffea73aa0
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2019-09-08 22:59:14 +0300

    Rename package

--- scripts/copy-files.js
+++ scripts/copy-files.js
@@ -58,7 +58,7 @@ function createPackageFile() {
     } = packageData;
 
     const minimalPackage = {
-      name: 'material-ui-new-context',
+      name: '@forabi/material-ui-new-context',
       author,
       version,
       description,
