21eb8dc2756289f2c6491a72cfa70436b6752223
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2019-09-10 16:51:59 +0300

    Fix package name and React version

--- scripts/copy-files.js
+++ scripts/copy-files.js
@@ -2,30 +2,18 @@
 import path from 'path';
 import fse from 'fs-extra';
 
-const files = [
-  'README.md',
-  'CHANGELOG.md',
-  'LICENSE',
-];
+const files = ['README.md', 'CHANGELOG.md', 'LICENSE'];
 
-Promise.all(
-  files.map((file) => copyFile(file))
-)
-.then(() => createPackageFile());
+Promise.all(files.map(file => copyFile(file))).then(() => createPackageFile());
 
 function copyFile(file) {
   const buildPath = resolveBuildPath(file);
-  return new Promise((resolve) => {
-    fse.copy(
-      file,
-      buildPath,
-      (err) => {
-        if (err) throw err;
-        resolve();
-      }
-    );
-  })
-  .then(() => console.log(`Copied ${file} to ${buildPath}`));
+  return new Promise(resolve => {
+    fse.copy(file, buildPath, err => {
+      if (err) throw err;
+      resolve();
+    });
+  }).then(() => console.log(`Copied ${file} to ${buildPath}`));
 }
 
 function resolveBuildPath(file) {
@@ -33,55 +21,59 @@ function resolveBuildPath(file) {
 }
 
 function createPackageFile() {
-  return new Promise((resolve) => {
-    fse.readFile(path.resolve(__dirname, '../package.json'), 'utf8', (err, data) => {
-      if (err) {
-        throw err;
+  return new Promise(resolve => {
+    fse.readFile(
+      path.resolve(__dirname, '../package.json'),
+      'utf8',
+      (err, data) => {
+        if (err) {
+          throw err;
+        }
+
+        resolve(data);
       }
-
-      resolve(data);
-    });
+    );
   })
-  .then((data) => JSON.parse(data))
-  .then((packageData) => {
-    const {
-      author,
-      version,
-      description,
-      keywords,
-      repository,
-      license,
-      bugs,
-      homepage,
-      peerDependencies,
-      dependencies,
-    } = packageData;
+    .then(data => JSON.parse(data))
+    .then(packageData => {
+      const {
+        author,
+        version,
+        description,
+        keywords,
+        repository,
+        license,
+        bugs,
+        homepage,
+        peerDependencies,
+        dependencies
+      } = packageData;
 
-    const minimalPackage = {
-      name: '@remotelock/material-ui-new-context',
-      author,
-      version,
-      description,
-      main: './index.js',
-      module: './index.es.js',
-      'jsnext:main': './index.es.js',
-      keywords,
-      repository,
-      license,
-      bugs,
-      homepage,
-      peerDependencies,
-      dependencies,
-    };
+      const minimalPackage = {
+        name: '@remotelock/material-ui',
+        author,
+        version,
+        description,
+        main: './index.js',
+        module: './index.es.js',
+        'jsnext:main': './index.es.js',
+        keywords,
+        repository,
+        license,
+        bugs,
+        homepage,
+        peerDependencies,
+        dependencies
+      };
 
-    return new Promise((resolve) => {
-      const buildPath = path.resolve(__dirname, '../build/package.json');
-      const data = JSON.stringify(minimalPackage, null, 2);
-      fse.writeFile(buildPath, data, (err) => {
-        if (err) throw (err);
-        console.log(`Created package.json in ${buildPath}`);
-        resolve();
+      return new Promise(resolve => {
+        const buildPath = path.resolve(__dirname, '../build/package.json');
+        const data = JSON.stringify(minimalPackage, null, 2);
+        fse.writeFile(buildPath, data, err => {
+          if (err) throw err;
+          console.log(`Created package.json in ${buildPath}`);
+          resolve();
+        });
       });
     });
-  });
 }
