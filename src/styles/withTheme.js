6ac95dcaf1f92d9a3410667dbab1dbe36b9ad5cd
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2019-09-13 01:31:45 +0300

    Always forward ref

--- src/styles/withTheme.js
+++ src/styles/withTheme.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import hoistNonReactStatics from 'hoist-non-react-statics';
 import Context from './Context';
-import isReact from 'is-react';
 
 export default function withTheme(Component) {
-  const WrappedComponent = React.forwardRef((props, _ref) => {
+  const WrappedComponent = React.forwardRef((props, ref) => {
     const context = React.useContext(Context);
-    const ref = isReact.classComponent(Component) ? _ref : undefined;
 
-    return <Component {...props} ref={ref} {...context} />;
+    return <Component ref={ref} {...props} {...context} />;
   });
 
   WrappedComponent.displayName = `withTheme(${Component.displayName ||
