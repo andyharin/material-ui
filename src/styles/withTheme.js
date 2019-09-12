206a5fb2a5aefc77362864ef3e29b16d2538a1b5
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2019-09-13 00:50:50 +0300

    Refactor withTheme to use useContext and fix ref forwarding warning

--- src/styles/withTheme.js
+++ src/styles/withTheme.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import hoistNonReactStatics from 'hoist-non-react-statics';
-import {Consumer} from './Context';
+import Context from './Context';
+import isReact from 'is-react';
 
-export default (Component) => {
-  const render = (innerRef, p) => (contexts) => {
-    if (!(contexts || {}).muiTheme) return null;
-    return <Component {...p} ref={innerRef} {...contexts} />;
-  };
-  const Wrapper = ({innerRef, ...p}) => <Consumer>{render(innerRef, p)}</Consumer>;
-  return hoistNonReactStatics(React.forwardRef((props, ref) => <Wrapper {...props} innerRef={ref} />), Component);
+export default Component => {
+  const WrappedComponent = React.forwardRef(function Component(_props, ref) {
+    const {muiTheme} = React.useContext(Context);
+    const props = {..._props};
+
+    if (isReact.classComponent(Component)) {
+      props.ref = ref;
+    }
+
+    return <Component {...props} muiTheme={muiTheme} />;
+  });
+
+  return hoistNonReactStatics(WrappedComponent, Component);
 };
